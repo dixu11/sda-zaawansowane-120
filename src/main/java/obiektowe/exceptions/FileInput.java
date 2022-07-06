35fcf992857b4ed82056b5a5caa12057c30058cb
Daniel Szlicht <daniel.szlicht25@gmail.com>
2022-07-06 20:22:07 +0200

    wczytywanie ksiazek

--- src/main/java/obiektowe/exceptions/FileInput.java
+++ src/main/java/obiektowe/exceptions/FileInput.java
@@ -9,21 +9,37 @@ import java.util.Scanner;
 public class FileInput {
 
 
-    public List<String> readBooks() {
-        List<String> booksList = new ArrayList<>();
+    public List<Book> readBooks() {
+        List<Book> booksList = new ArrayList<>();
         File file = new File("src\\main\\java\\obiektowe\\exceptions\\ksiazki.txt");
-        try{
+        try {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
-                String book = scanner.nextLine();
+                String line = scanner.nextLine();
+                Book book = mapLineToBook(line);
                 booksList.add(book);
             }
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("Nie znaleziono pliku!");
             e.printStackTrace();
         }
 
         return booksList;
+    }
+
+    private Book mapLineToBook(String line) {
+        try{
+        String[] elements = line.split(";");
+        String title = elements[0];
+        int year = Integer.parseInt(elements[1]);
+        Book book = new Book(title, year);
+            return book;
+        }catch (Exception exception){
+            System.out.println("Niepoprawny format pliku!!!!");
+            System.out.println("Dla linii: " + line);
+            return null;
+        }
+
     }
 
 
