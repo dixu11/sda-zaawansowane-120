821856979f113260acf98b4b067931b9845e9876
Daniel Szlicht <daniel.szlicht25@gmail.com>
2022-07-07 20:58:10 +0200

    add test

--- src/test/java/obiektowe/collections/MyArrayListTest.java
+++ src/test/java/obiektowe/collections/MyArrayListTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyArrayListTest {
 
-   private List<Integer> elements = new MyArrayList<>();
+    private List<Integer> elements = new ArrayList<>();
 
     @Test
     public void addShouldHandleFewElements() {
@@ -16,11 +17,11 @@ class MyArrayListTest {
         elements.add(11);
         elements.add(13);
 
-        assertEquals(3,elements.size());
+        assertEquals(3, elements.size());
     }
 
     @Test
-    public void addShouldHandleManyElements(){
+    public void addShouldHandleManyElements() {
         elements.add(23);
         elements.add(11);
         elements.add(13);
@@ -33,13 +34,13 @@ class MyArrayListTest {
         elements.add(13);
         elements.add(13);//11 elements
 
-        assertEquals(11,elements.size());
+        assertEquals(11, elements.size());
     }
 
     @Test
     public void itsImpossibleToAddNullValue() {
         elements.add(null);
-        assertEquals(0,elements.size());
+        assertEquals(0, elements.size());
     }
 
     @Test
@@ -47,7 +48,7 @@ class MyArrayListTest {
         try {
             elements.get(3);
 //            elements.get(13);
-        }catch (Exception e){
+        } catch (Exception e) {
             assertEquals(IndexOutOfBoundsException.class, e.getClass());
             return;
         }
@@ -64,10 +65,10 @@ class MyArrayListTest {
         elements.add(1, 9);
 
         assertEquals(5, elements.size());
-        assertEquals(9,elements.get(1));
-        assertEquals(3,elements.get(2));
-        assertEquals(4,elements.get(3));
-        assertEquals(8,elements.get(4));
+        assertEquals(9, elements.get(1));
+        assertEquals(3, elements.get(2));
+        assertEquals(4, elements.get(3));
+        assertEquals(8, elements.get(4));
     }
     // 6 , 3, 4, 8    = 4 actualSize
     // 0   1  2  3
@@ -76,6 +77,20 @@ class MyArrayListTest {
     // 6 , 3, 4, 4, 8
     // 6 , 3, 3, 4, 8
     // 6 , 9, 3, 4, 8
+
+    @Test
+    public void shouldFindMatchingElement() {
+        elements.add(6);
+        elements.add(3);
+        elements.add(4);
+        elements.add(8);
+
+        boolean shouldFind = elements.contains(3);
+        boolean shouldNotFind = elements.contains(10);
+
+        assertTrue(shouldFind);
+        assertFalse(shouldNotFind);
+    }
 
 
 }
