1e66531016729346555db67f218af8a8b616b281
Daniel Szlicht <daniel.szlicht25@gmail.com>
2022-07-26 18:15:25 +0200

    bubble sort

--- src/main/java/obiektowe/collections/restaurant/Main.java
+++ src/main/java/obiektowe/collections/restaurant/Main.java
@@ -1,16 +1,18 @@
 package obiektowe.collections.restaurant;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant();
-        Dish dish = new Dish("spaghetti",33,DishType.DINNER,500);
-        Dish dish2 = new Dish("jajecznica",20,DishType.BREAKFAST,350);
-        Dish dish3 = new Dish("pomidorowa",18,DishType.APPETIZER,250);
+        Dish dish = new Dish("spaghetti", 33, DishType.DINNER, 500);
+        Dish dish2 = new Dish("jajecznica", 20, DishType.BREAKFAST, 350);
+        Dish dish3 = new Dish("pomidorowa", 18, DishType.APPETIZER, 250);
         restaurant.addDish(dish);
         restaurant.addDish(dish2);
         restaurant.addDish(dish3);
         System.out.println(restaurant);
-
-
     }
 }
---
+++ src/main/java/obiektowe/collections/sort/SortingAlgorithm.java
@@ -0,0 +1,38 @@
+package obiektowe.collections.sort;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SortingAlgorithm {
+
+
+    public void sort(List<Integer> numbers) {
+        for (int j = 0; j < numbers.size(); j++) {
+            for (int i = 0; i < numbers.size() - 1 - j; i++) {
+                int first = numbers.get(i);
+                int second = numbers.get(i+1);
+                if (first > second) {
+                    numbers.set(i, second);
+                    numbers.set(i + 1, first);
+                }
+            }
+        }
+    }
+
+
+}
+
+class SortingDemo {
+
+    public static void main(String[] args) {
+        SortingAlgorithm sortingAlgorithm = new SortingAlgorithm();
+        List<Integer> numbers = new LinkedList<>(List.of(12, 34, 9, 55, 44, 1, 7, 99, 2, 44)); //10 liczb
+        System.out.println(numbers);
+        sortingAlgorithm.sort(numbers);
+        System.out.println(numbers);
+
+
+    }
+
+}
